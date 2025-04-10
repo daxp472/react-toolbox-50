540a58626f500c8d4d8454e25cd4984dc8260f4e
daxp472 <daxpatel.cg@gmail.com>
2025-04-10 19:07:30 +0530

    Updated App.jsx for the recent NavIcon file changes

--- Day 03/src/App.jsx
+++ Day 03/src/App.jsx
@@ -1,5 +1,6 @@
 import { FaRocket, FaStar } from 'react-icons/fa'
 import { MdSettings } from 'react-icons/md'
+import NavIcon from './components/icons/NavIcon'
 import './App.css'
 
 function App() {
@@ -7,9 +8,11 @@ function App() {
     <div className="app-container">
       <h1>React Icons Demo</h1>
       <div className="icon-examples">
+        <NavIcon />
         <FaRocket size={40} color="#e74c3c" className="spin" />
         <FaStar size={30} color="#f1c40f" />
         <MdSettings size={50} color="#3498db" />
+        
       </div>
     </div>
   )
