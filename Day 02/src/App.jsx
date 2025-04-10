f0fc132e212d5b74dbc93b4331c1354275c8bc5b
daxp472 <daxpatel.cg@gmail.com>
2025-04-10 12:28:05 +0530

    UI updated with some extra css

--- Day 02/src/App.jsx
+++ Day 02/src/App.jsx
@@ -6,6 +6,7 @@ import FeedbackCopyDemo from './components/FeedbackCopyDemo';
 
 function App() {
   return (
+    <div className="app">
     <div className="app-container">
       <h1>React Clipboard Demo</h1>
       <section className="demo-section">
@@ -13,6 +14,7 @@ function App() {
         <InputCopyDemo />
         <FeedbackCopyDemo />
       </section>
+    </div>
     </div>
   );
 }
